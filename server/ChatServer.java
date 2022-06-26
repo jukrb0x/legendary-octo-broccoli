e57e25a8b35fec29b1c5cf9a8f61ac67e480db8f
Denver <denverleejy@gmail.com>
2022-06-27 03:39:45 +0800

    Solve showing double timeline

--- server/ChatServer.java
+++ server/ChatServer.java
@@ -73,7 +73,6 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerIF {
         try {
             java.rmi.registry.LocateRegistry.createRegistry(portNumber);
             String message = "RMI Registry started on port " + portNumber + "\n";
-            jta.append(message);
             System.out.println(message);
         } catch (RemoteException e) {
             e.printStackTrace();
